281f976d43419bb77ffd9e85f42e147af58bfb31
dan <danburk10+github@gmail.com>
2023-05-29 21:44:31 -0500

    original version

---
+++ heic2png.py
@@ -0,0 +1,15 @@
+' batch convert HEIC to png
+' from cli: ls -name *.HEIC | python .\io.py
+
+' requires pillow, pillow_heif
+
+import sys
+from PIL import Image
+import pillow_heif
+
+pillow_heif.register_heif_opener()
+
+for line in sys.stdin:
+    filename = line.strip()
+    img = Image.open(filename)
+    img.save(filename+'.png', format('png'))
